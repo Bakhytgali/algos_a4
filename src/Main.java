99863fe4c2625eb0db3a2fb4e34bc797632a8202
Your Name <omgwtfwat@gmail.com>
2023-05-14 21:38:54 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        MyHashTable<MyTestingClass, String> table = new MyHashTable<>();
+        for (int i = 0; i<10000; i++){
+            MyTestingClass key  = new MyTestingClass(i);
+            table.put(key, "value" + i);}
     }
 }
--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 public class MyHashTable {
     private class HashCode<K, V> {
         private K key;
@@ -14,8 +16,33 @@ public class MyHashTable {
             return "{" + key + " " + value + "}";
 
         }
+
+        public K getKey() {
+            return key;
+        }
+
+        public void setKey(K key) {
+            this.key = key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
     }
-    private HashCode[] chainArray; //array of Hash Nodes
+    private List<HashCode<K, V>>[] chainArray; //array of Hash Nodes
     private int numOfBuckets; // capacity
     private int size; // size
+    public MyHashTable(int capacity){
+        this.numOfBuckets = capacity;
+        chainArray = new ArrayList[capacity];
+        size = 0;
+    }
+    public void put(K key, V value){
+        int index = getIndex(key);
+        List<HashCode<K, V>>
+    }
 }
