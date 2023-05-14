eefa7add948547620b2ec1319673a2ae423ecdd3
Your Name <omgwtfwat@gmail.com>
2023-05-14 19:06:14 +0600

    commit

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,15 +1,21 @@
 public class MyHashTable {
-    private class HashCode<K, V>{
+    private class HashCode<K, V> {
         private K key;
         private V value;
         private HashCode<K, V> next;
-        public HashCode(K key, V value){
+
+        public HashCode(K key, V value) {
             this.key = key;
             this.value = value;
         }
+
         @Override
-        public String toString(){
+        public String toString() {
             return "{" + key + " " + value + "}";
+
         }
     }
+    private HashCode[] chainArray; //array of Hash Nodes
+    private int numOfBuckets; // capacity
+    private int size; // size
 }
