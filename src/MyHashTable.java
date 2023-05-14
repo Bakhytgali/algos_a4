428f20e0cd2b8524cc43937746704cd4e13ae4cf
Your Name <omgwtfwat@gmail.com>
2023-05-14 21:53:08 +0600

    commit

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,20 +1,95 @@
 import java.util.ArrayList;
 import java.util.List;
-public class MyHashTable {
-    private class HashCode<K, V> {
+
+
+public class MyHashTable<K, V> {
+    private static final int capacity = 16; // capacity  = variable that shows the capacity of the hashTable
+    private static final double load_factor = 0.75; // load_factor  = variable that increases the size of the hashTable
+
+    private List<Entry<K,V>>[] buckets; // buckets = variable that defines buckets of the hashTable
+    private int size; // size = size of the hashTable
+
+    public MyHashTable(){ // MyHashTable  = constructor that has capacity
+        this(capacity);
+    }
+
+    public MyHashTable(int in_capacity) {
+        buckets = new ArrayList[in_capacity];
+        size = 0;
+    }
+
+    public void put(K key, V value) { // use to put elements into hashTable
+        int index = getIndex(key);
+        List<Entry<K,V>> bucket = getBucket(index);
+        for (Entry<K,V> entry: bucket) {
+            if (entry.getKey().equals(key)){
+                entry.setValue(value);
+                return;
+            }
+        }
+        bucket.add(new Entry<>(key,value));
+        size++;
+        if (size> buckets.length * load_factor){
+            resize();
+        }
+    }
+
+    public V get(K key){ // to get the index
+        int index = getIndex(key);
+        List<Entry<K, V>> bucket = getBucket(index);
+        for (Entry<K,V> entry: bucket) {
+            if (entry.getKey().equals(key)){
+                return entry.getValue();
+            }
+        }
+        return null;
+    }
+
+    public void remove(K key){ // to remove element
+        int index = getIndex(key);
+        List<Entry<K, V>> bucket = getBucket(index);
+        for (Entry<K,V> entry: bucket){
+            if (entry.getKey().equals(key)){
+                bucket.remove(entry);
+                size--;
+                return;
+            }
+        }
+    }
+
+    private int getIndex(K key){
+        int hashCode = key.hashCode();
+        return Math.abs(hashCode % buckets.length);
+    }
+
+    private List<Entry<K,V>> getBucket(int index){
+        if (buckets[index] == null){
+            buckets[index] = new ArrayList<>();
+        }
+        return buckets[index];
+    }
+
+
+    private void resize(){
+        List<Entry<K,V>>[] oldBuckets = buckets;
+        buckets = new List[buckets.length * 2];
+        size = 0;
+        for (List<Entry<K,V>> bucket: oldBuckets){
+            if (bucket != null) {
+                for (Entry<K,V> entry: bucket) {
+                    put(entry.getKey(), entry.getValue());
+                }
+            }
+        }
+    }
+
+    private static class Entry<K,V> {
         private K key;
         private V value;
-        private HashCode<K, V> next;
 
-        public HashCode(K key, V value) {
+        public Entry(K key, V value) {
             this.key = key;
             this.value = value;
-        }
-
-        @Override
-        public String toString() {
-            return "{" + key + " " + value + "}";
-
         }
 
         public K getKey() {
@@ -32,17 +107,5 @@ public class MyHashTable {
         public void setValue(V value) {
             this.value = value;
         }
-    }
-    private List<HashCode<K, V>>[] chainArray; //array of Hash Nodes
-    private int numOfBuckets; // capacity
-    private int size; // size
-    public MyHashTable(int capacity){
-        this.numOfBuckets = capacity;
-        chainArray = new ArrayList[capacity];
-        size = 0;
-    }
-    public void put(K key, V value){
-        int index = getIndex(key);
-        List<HashCode<K, V>>
     }
 }
--- src/MyTestingClass.java
+++ src/MyTestingClass.java
@@ -1,2 +1,22 @@
 public class MyTestingClass {
+    private  int value;
+
+    public MyTestingClass(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public int hashCode(){
+        int result = 17;
+        result = 31*result + value;
+        return result;
+    }
 }
